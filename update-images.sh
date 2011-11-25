58f25557af0e0d9b6bdb33d456861edb8db7e378
Bastien Durel <bastien@durel.org>
2011-11-25 20:47:25 +0100

    update-(prod|images) pour git

--- update-images.sh
+++ update-images.sh
@@ -7,4 +7,20 @@ if ! id -un | grep -q starshine ; then
     echo Error: you must be starshine
     exit 1
 fi
-cd prod/image ; svn update
+
+if [ -z "$1" ] ; then
+		target=master
+else
+		target="$1"
+fi
+
+REPO=/srv/starshine-data/git_repo/Starshine-images
+PROD=/srv/starshine-data/starshine/image
+
+set -x
+
+cd $REPO
+git fetch || exit 1
+git reset --soft refs/remotes/origin/master || exit 1
+git archive $target | tar -x -C $PROD
+exit 0
--- update-prod.sh
+++ update-prod.sh
@@ -7,30 +7,20 @@ if ! id -un | grep -q starshine ; then
     echo Error: you must be starshine
     exit 1
 fi
-function do_patch
-{
-    url="http://tools.assembla.com/svn/starshine/tags/$1"
-    svn sw $url prod
-    exit 0
-}
-echo Recherche des tags disponibles :
-TAGS=`svn ls http://tools.assembla.com/svn/starshine/tags`
-echo $TAGS
-if [ -z $1 ]; then
-    echo -n "Quel tag voulez-vous mettre en production ? "
-    read TAG
+
+if [ -z "$1" ] ; then
+		target=master
 else
-    TAG=$1
+		target="$1"
 fi
-for pos in $TAGS ; do
-    if [ "$TAG/" = "$pos" ]; then
-        echo ok
-        do_patch $pos
-    fi
-    if [ "$TAG" = "$pos" ]; then
-	echo ok
-	do_patch $pos
-    fi
-done
-echo Tag invalide, reessayez
-exit 1
+
+REPO=/srv/starshine-data/git_repo/Starshine-Online
+PROD=/srv/starshine-data/starshine/starshine071
+
+set -x
+
+cd $REPO
+git fetch || exit 1
+git reset --soft refs/remotes/origin/master || exit 1
+git archive $target | tar -x -C $PROD
+exit 0
