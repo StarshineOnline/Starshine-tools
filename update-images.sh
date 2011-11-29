dd56607c51be78afd35253810af9048716ce03e7
Bastien Durel <bastien@durel.org>
2011-11-29 20:48:49 +0100

    on a pas besoin d'un repo bare pour les images, donc on peut utiliser le repo pour le serveur web, ce sera plus rapide que le git archive

--- update-images.sh
+++ update-images.sh
@@ -8,19 +8,15 @@ if ! id -un | grep -q starshine ; then
     exit 1
 fi
 
-if [ -z "$1" ] ; then
-		target=master
-else
-		target="$1"
-fi
-
-REPO=/srv/starshine-data/git_repo/Starshine-images
 PROD=/srv/starshine-data/starshine/image
 
 set -x
 
-cd $REPO
-git fetch || exit 1
-git reset --soft refs/remotes/origin/master || exit 1
-git archive $target | tar -x -C $PROD
+cd $PROD
+git pull
+if [ ! -z "$1" ] ; then
+	git checkout $1
+fi
+
 exit 0
+
